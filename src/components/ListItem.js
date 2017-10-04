8e60d54e0bc7273f480f6e3a5297088b276a8222
Nikita Chernyshov <xpom55@mail.ru>
2017-10-04 18:12:31 +0300

    set action

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -5,6 +5,18 @@ import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component {
+  renderDescription() {
+    const { library, selectedLibraryId } = this.props;
+
+    console.log(selectedLibraryId);
+
+    if (library.id === selectedLibraryId) {
+      return (
+        <Text>{library.description}</Text>
+      );
+    }
+  }
+
   render() {
     const { titelStyle } = styles;
     const { id, title } = this.props.library;
@@ -19,6 +31,7 @@ class ListItem extends Component {
               {title}
             </Text>
           </CardSection>
+          {this.renderDescription()}
         </View>
       </TouchableWithoutFeedback>
     );
@@ -33,4 +46,8 @@ const styles = {
   }
 };
 
-export default connect(null, actions)(ListItem);
+const mapStateToProps = state => {
+  return { selectedLibraryId: state.selectedLibraryId };
+};
+
+export default connect(mapStateToProps, actions)(ListItem);
